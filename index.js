2b35f99ed71b097b1ebe7399188175ecd06fcc0a
EL5225 <luthfiramadhan155@gmail.com>
2023-09-27 21:22:37 +0700

    feature: update crud

--- index.js
+++ index.js
@@ -3,17 +3,21 @@ const { create, index, show, update, destroy } = require("./helpers/crud");
 async function main() {
   try {
     // test create post
-    // let newPost = await create("test title 2", "test data 2");
-    // console.log(newPost);
+    let newPost = await create("test title", "test data");
+    console.log(newPost);
+
     // // test show all post
     // let posts = index();
     // console.log(posts);
+
     // // test show detail post by id
     // let post = await show(1);
     // console.log(post);
+
     // // test update post by id
-    let updatedPost = await update(2, "updated title 2", "updated data 2");
-    console.log(updatedPost);
+    // let updatedPost = await update(1, 'title baru dua');
+    // console.log(updatedPost);
+
     // // test delete post by id
     // let deletedPost = await destroy(1);
     // console.log(deletedPost);
